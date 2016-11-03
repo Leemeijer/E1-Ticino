8731cfffaa59c1309252cad67333e7bb369e0f82
Leemeijer <b.leemeijer@hotmail.com>
2016-11-03 15:22:40 +0100

    GPS knop en meerdere menu lagen toegevoegd

--- js/Script.js
+++ js/Script.js
@@ -23,8 +23,6 @@ var
     ViaF = L.geoJson(null, {style:style, onEachFeature:onEachFeature}),
     Aanbevolen = L.geoJson(null, {style:style});
 
- Hoofdroute.setZIndex(1000);
-
 //Variabelen van de Points of Interest:
     Stations = L.geoJson(null, {
                 pointToLayer: function(feature, latlng){
@@ -50,6 +48,12 @@ var
                 });
 
 //__________________________________________________________________________________________________________________      
+                                        //Esri basemap toevoegen:
+
+var Esri = L.esri.basemapLayer('Topographic');
+//Esri Basemaps zijn onder anderen: Topographic, Imagery, NationalGeographic, Streets, Oceans, Gray, DarkGray, SchadedRelief
+
+//__________________________________________________________________________________________________________________      
                                         //GeoJsons oproepen:
 //Routes:
     jQuery.getJSON("GeoJson/E1_hoofdroute_italie.json", function (data) { Hoofdroute.addData(data)}),
@@ -65,7 +69,7 @@ var
 
 //__________________________________________________________________________________________________________________      
                                         //Maken van de kaart:
-//Map + layers + attributen
+//Map + layers + eigenschappen 
     var map = L.map('map', {layers: [osm, Hoofdroute], 
             minZoom: 9,
             maxBounds: [[46.050361, 8.1672119140625],
@@ -73,24 +77,36 @@ var
                         [44.991221, 9.698682], 
                         [46.050361, 9.698682] ]
     }).setView([45.654464,  9.164932], 10);
-map.locate({setView: true, maxZoom: 16});
 
-
-var Esri = L.esri.basemapLayer('Topographic');
-//Esri Basemaps zijn onder anderen: Topographic, Imagery, NationalGeographic, Streets, Oceans, Gray, DarkGray, SchadedRelief
-
+//Met de regel hieronder voeg je de locatie button toe
+L.control.locate({position: 'bottomright'}).addTo(map);
 
 //__________________________________________________________________________________________________________________  
-                                        //Popups voor de lijnen: 
+                                        //Markers
+    var pont = new L.LayerGroup();
+    L.marker([45.9846512, 8.94589019]).addTo(pont),
+    L.marker([45.9040536, 8.90070097]).addTo(pont);
+
+    var impressies = new L.LayerGroup();
+    L.marker([45.465526, 8.885021]).addTo(impressies).bindPopup('<b>Magenta</b> <div> <img style="width:80px" src="images/Magenta.png" /></div>');
+
+    var horeca = new L.LayerGroup();
+    L.marker([45.206231, 9.015038]).addTo(horeca);
+    
+    var vragen = new L.LayerGroup();
+    L.marker([45.342898, 8.880618]).addTo(vragen).bindPopup('<b>Gevaarlijke brug</b><br> Brug bij SP494 nabij Vigevano</br> <div> <img style="width:150px" src="images/brug.jpg" /></div>');
+
+//__________________________________________________________________________________________________________________  
+                                        //Popups: 
 //Vernoem je variabele en zet er '.bindPopup' achter om de popup te maken. Alles tussen de haakjes () is wat je te zien krijgt.
 
+//Routelijnen:
 Hoofdroute.bindPopup('<b>German Site for E1:</b> <a target="_blank" href="https://e1.hiking-europe.eu/">Hiking Europe</a> <br> <b>All hiking trails in Europe:</b> <a target="_blank" href="http://waymarkedtrails.org/">Waymarked Trails</a>')
 LokaleVariant.bindPopup('<a target="_blank" href="http://web.archive.org/web/20160405141619/http://www.enrosadira.it/e1/">Local E1 Alternatives</a>')
 ViaF.bindPopup('<b>Site:</b> <br> <a target="_blank" href="http://www.dewegvandefranken.nl/">Via Francigena</a>')
 Aanbevolen.bindPopup('Recommended bij BosmaGrafiek.nl for a part of the pilgrimage from the St. Gottthard pass to Roma')
 Swiss02.bindPopup('<b>Site:</b> <a target="_blank" href="http://www.wanderland.ch/en/routes/route-02.html">Trans Swiss Trail</a> <br> <b>App:</b> Search for <i>Switzerland Mobility</i>')
 Swiss07.bindPopup('<b>Site:</b> <a target="_blank" href="http://www.wanderland.ch/en/routes/route-07.html">Via Gottardo</a> <br> <b>App:</b> Search for <i>Switzerland Mobility</i>');
-
 
 //__________________________________________________________________________________________________________________  
                                         //Lagen menu toevoegen:  
@@ -125,28 +141,58 @@ Swiss07.bindPopup('<b>Site:</b> <a target="_blank" href="http://www.wanderland.c
             }
         },
         
-        {
-                    groupName: "test Trails",
+        
+                    {
+                    groupName: "Traffic Info",
                     expanded: true,
                     layers: {
+            "Pedestrian Bridges"        : Bruggen,
+            "Train Stations"            : Stations,
+            "Ferry and funicular"       : pont
+            }
+        },
+    
+                    {
+                    groupName: "Tourist Info",
+                    expanded: true,
+                    layers: {
+            "Scenery"                   : impressies,
+            "Eat & sleep"               : horeca
             
             }
         },
         
                     {
-                    groupName: "POI's",
-                    expanded: true,
+                    groupName: "Survey",
+                    expanded: false,
                     layers: {
-            "Pedestrian Bridges"        : Bruggen,
-            "Train Stations"            : Stations     
+            "Questions"                 : vragen
+            
             }
-        }];
+        }
+    
+    
+    ];
 
 
 //Lagen menu
     var control= L.Control.styledLayerControl(kaarten, data).addTo(map);
 
 //opmerking: Alles wat je in deze Layer Control wilt hebben moet je boven deze functie plaatsen anders gaat het mis.
+
+
+
+//__________________________________________________________________________________________________________________ 
+                                        //Titel 
+
+var titel = L.control({position: 'topleft'});
+
+titel.onAdd = function (map) {
+    var div = L.DomUtil.create('div');
+    div.innerHTML = '<div> <img style="width:90%; height:100%" src="images/E1_ticino_title_up01_rgb[301].png"/> </div>';
+   return div;
+};
+titel.addTo(map);
 
 //__________________________________________________________________________________________________________________ 
                                         //Legenda
@@ -163,27 +209,14 @@ legend.addTo(map);
 //__________________________________________________________________________________________________________________ 
                                         //Schaalbalk
 
-// add a scale at at your map.
-var scale = L.control.scale({position: 'bottomright'}).addTo(map); 
+// haal de '//' weg voor .addTo(map) in de schaalbalk weer aan te zetten.
+var scale = L.control.scale({position: 'bottomright'})//.addTo(map); 
 
 // Get the label.
 var metres = scale._getRoundNum(map.containerPointToLatLng([0, map.getSize().y / 2 ]).distanceTo( map.containerPointToLatLng([scale.options.maxWidth,map.getSize().y / 2 ])))
   label = metres < 1000 ? metres + ' m' : (metres / 1000) + ' km';
 
   console.log(label);
-
-//__________________________________________________________________________________________________________________ 
-                                        //Titel 
-
-var titel = L.control({position: 'topleft'});
-
-titel.onAdd = function (map) {
-    var div = L.DomUtil.create('div');
-    div.innerHTML = '<div> <img style="width:90%; height:100%" src="images/E1_ticino_title_up01_rgb[301].png"/> </div>';
-   return div;
-};
-titel.addTo(map);
-
 
 //__________________________________________________________________________________________________________________  
                                         //Functies:
@@ -260,13 +293,3 @@ var gevaar = L.icon ({
         iconSize: [25, 25],
         iconAnchor: [0,0]
      });
-
-//__________________________________________________________________________________________________________________ 
-                                        //Losse markers
-
-//Marker met eigen icoon 
-L.marker([45.342898, 8.880618], {icon: gevaar}).addTo(map).bindPopup('<b>Gevaarlijke brug</b><br> Brug bij SP494 nabij Vigevano</br> <div> <img style="width:150px" src="images/brug.jpg" /></div>');
-
-//Leaflet marker met popup (Magenta)
-var magenta = L.marker([45.465526, 8.885021]).addTo(map);
-    magenta.bindPopup('<b>Magenta</b> <div> <img style="width:80px" src="images/Magenta.png" /></div>');
